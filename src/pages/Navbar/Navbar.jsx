bbfc31b81282d0562f730fec7b5fb25ebcd3f8c9
Arjun <arjunkhare71@gmail.com>
2021-10-29 01:02:08 +0530

    added background colour and removed scrollbar

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -19,31 +19,33 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="navbar">
-                <div className="navbar__logo">
-                    <img src="images/logo.png" alt="logo" />
-                </div>   
-                <div className="navbar__search">
-                    <div className="navbar__search-inputContainer">
-                        <input placeholder="Search" />
+            <div className="parent">
+                <div className="navbar">
+                    <div className="navbar__logo">
+                        <img src="images/logo.png" alt="logo" />
+                    </div>   
+                    <div className="navbar__search">
+                        <div className="navbar__search-inputContainer">
+                            <input placeholder="Search" />
+                        </div>
                     </div>
+                    <div className="navbar__icons">
+                        <Icon imgpath = {Home} />
+                        <div className="navbar__icons-message">
+                            <Icon imgpath = {Msg} />
+                            <div>2</div>
+                        </div>
+                        <Icon imgpath = {Plus} />
+                        <Icon imgpath = {Accessibility} />
+                        <Icon imgpath = {Like} />
+                        <div className="navbar__icons-profile" onClick={handleclick}>
+                            <img src="images/profile.jpg" alt="profile pic" />
+                        </div>
+                    </div>
+                    {click && <Profiledropdown />}
                 </div>
-                <div className="navbar__icons">
-                    <Icon imgpath = {Home} />
-                    <div className="navbar__icons-message">
-                        <Icon imgpath = {Msg} />
-                        <div>2</div>
-                    </div>
-                    <Icon imgpath = {Plus} />
-                    <Icon imgpath = {Accessibility} />
-                    <Icon imgpath = {Like} />
-                    <div className="navbar__icons-profile" onClick={handleclick}>
-                        <img src="images/profile.jpg" alt="profile pic" />
-                    </div>
-                </div>
-                {click && <Profiledropdown />}
-            </div>
             <div className="fakebox"></div>
+            </div>
         </>
     )
 }
